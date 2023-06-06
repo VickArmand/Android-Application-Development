ef34704fc5176442e33dab029846323c93a6095b
VickArmand <victormaina1962@gmail.com>
2023-06-06 15:08:26 +0300

    Adding Toasts

--- Activity Lifecycle/app/src/main/java/com/example/activitylifecycle/MainActivity.java
+++ Activity Lifecycle/app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,41 +14,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // logging a message in the logcat using Log.d
         Log.d("Life cycle Event", "onCreate event");
+        Toast.makeText(getApplicationContext(), "onCreate event", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.d("Life cycle Event", "onStart event");
+        Toast.makeText(getApplicationContext(), "onStart event", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Log.d("Life cycle Event", "onResume event");
+        Toast.makeText(getApplicationContext(), "onResume event", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Log.d("Life cycle Event", "onPause event");
+        Toast.makeText(getApplicationContext(), "onPause event", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         Log.d("Life cycle Event", "onStop event");
+        Toast.makeText(getApplicationContext(), "onStop event", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.d("Life cycle Event", "onDestroy event");
+        Toast.makeText(getApplicationContext(), "onDestroy event", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
         Log.d("Life cycle Event", "onRestart event");
+        Toast.makeText(getApplicationContext(), "onRestart event", Toast.LENGTH_SHORT).show();
     }
 }
