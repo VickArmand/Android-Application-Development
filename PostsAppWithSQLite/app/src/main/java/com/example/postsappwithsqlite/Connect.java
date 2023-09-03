9ad4b626b26492aedf8957a0d1d1260fdfec8b05
VickArmand <victormaina1962@gmail.com>
2023-09-03 16:15:04 +0300

    modify files + Java + MongoDB

--- PostsAppWithSQLite/app/src/main/java/com/example/postsappwithsqlite/Connect.java
+++ PostsAppWithSQLite/app/src/main/java/com/example/postsappwithsqlite/Connect.java
@@ -1,2 +1,31 @@
-package com.example.postsappwithsqlite;public class Connect {
+package com.example.postsappwithsqlite;
+
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connect {
+    private final String Dbname = "db_blogs.db";
+//    private String current_path = Paths.get("/").toAbsolutePath().toString();
+    //    OR
+//    private String current_path = FileSystems.getDefault().getPath("/").toAbsolutePath().toString();
+//    private final String ConnectionString = "jdbc:sqlite:"+current_path+Dbname;
+    private final String ConnectionString = "jdbc:sqlite:"+Dbname;
+    public Connection c = null;
+
+    public Connection Connect() throws SQLException {
+        try
+        {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection(ConnectionString);
+            System.out.println("Connection generated successfully");
+        }
+        catch (Exception ex)
+        {
+            System.out.println("Connection Failed: "+ ex.getMessage());
+            System.exit(0);
+        }
+        return c;
+    }
 }
--- PostsAppWithSQLite/app/src/main/java/com/example/postsappwithsqlite/CreateDB.java
+++ PostsAppWithSQLite/app/src/main/java/com/example/postsappwithsqlite/CreateDB.java
@@ -1,2 +1,27 @@
-package com.example.postsappwithsqlite;public class CreateDB {
+package com.example.postsappwithsqlite;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CreateDB extends Connect{
+    private Statement stmt;
+    private String create = "CREATE TABLE posts (id PRIMARY KEY AUTOINCREMENT NOT NULL, title CHAR(50) NOT NULL, slug CHAR(50) NOT NULL, body TEXT NOT NULL)";
+    private Connection c;
+    protected void createDB() throws SQLException {
+        try
+        {
+            c = Connect();
+            stmt = c.createStatement();
+            stmt.executeQuery(create);
+            stmt.close();
+            c.close();
+            System.out.println("Table created successfully");
+        }
+        catch(Exception ex)
+        {
+            System.out.println("Error creating table: "+ ex);
+            System.exit(0);
+        }
+    }
 }
